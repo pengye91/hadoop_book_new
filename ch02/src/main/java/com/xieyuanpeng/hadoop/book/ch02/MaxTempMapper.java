eda0fb0ccb63dc113d362c0bd294d2aebc9ba25a
xieyuanpeng <xieyuanpeng@zhihu.com>
2018-11-22 20:08:48 +0800

    first commit

---
+++ ch02/src/main/java/com/xieyuanpeng/hadoop/book/ch02/MaxTempMapper.java
@@ -0,0 +1,12 @@
+package com.xieyuanpeng.hadoop.book.ch02;
+
+import org.apache.hadoop.mapreduce.Mapper;
+
+/**
+ * @author xyp
+ * @date 2018/11/21
+ **/
+public class MaxTempMapper extends Mapper<> {
+
+}
+
