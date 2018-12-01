3959d05f9301608cebb919302d0bef9db705aa5f
xieyuanpeng <xieyuanpeng@zhihu.com>
2018-12-01 18:06:31 +0800

    added MaxTempMapper

--- ch02/src/main/java/com/xieyuanpeng/hadoop/book/ch02/MaxTempMapper.java
+++ ch02/src/main/java/com/xieyuanpeng/hadoop/book/ch02/MaxTempMapper.java
@@ -1,12 +1,26 @@
 package com.xieyuanpeng.hadoop.book.ch02;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
 
 /**
  * @author xyp
  * @date 2018/11/21
  **/
-public class MaxTempMapper extends Mapper<> {
+public class MaxTempMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+    public static final int MISSING = 9999;
 
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] lineWords = value.toString().split("\\s+");
+        String year = lineWords[0];
+        int airTemp = Integer.parseInt(lineWords[4]);
+        if (airTemp != MISSING) {
+            context.write(new Text(year), new LongWritable(airTemp));
+        }
+    }
 }
 
---
+++ ch02/src/main/java/com/xieyuanpeng/hadoop/book/ch02/MaxTempReducer.java
@@ -0,0 +1,8 @@
+package com.xieyuanpeng.hadoop.book.ch02;
+
+/**
+ * @author xyp
+ * @date 2018/12/01
+ **/
+public class MaxTempReducer {
+}
